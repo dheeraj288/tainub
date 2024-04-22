4b0af5970c442e2afc58cab0801d12852382a763
dheerajkalwar <dheeraj@example.com>
2024-04-22 16:28:18 +0530

    Add wishlist model and property controller and view and add column to property model and create actio text to implement my bin

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Ruby on Rails - 2024 Trainub Clone - Ruby , Rails , HTML, CSS , JavaSript , TailwindCSS, PostgreSQL
+
+
+
+Part 26 -  Reservation table
+
+A user will reseve/book properly
+
+user_id
+property_id
+checking_date
+checkout_date
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "el-transition" # @0.0.7
 pin 'swiper', to: 'https://cdn.jsdelivr.net/npm/swiper@11.0.5/dist/js/swiper.min.js'
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
